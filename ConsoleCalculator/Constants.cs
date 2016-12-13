9dae011ed996d64abd33e56a2902388eb26db2d5
EvgeniiDobrynin <eugenemmk@gmail.com>
2016-12-13 22:42:26 +0600

    Update Constants.cs

--- ConsoleCalculator/Constants.cs
+++ ConsoleCalculator/Constants.cs
@@ -10,8 +10,9 @@ namespace ConsoleCalculator
     public static class Constants
     {
         public static readonly string decimalDelimiter = Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator;
-        public static readonly string validator = "0123456789.," + Operations.priority.Select(k => k.Key).Aggregate((current, next) => current + next);
-
+        public static readonly string operations = Operations.priority.Select(k => k.Key).Aggregate((current, next) => current + next);
+        public static readonly string validator = "0123456789.," + operations;
+        
         public static readonly Dictionary<string, string> specialCommands = new Dictionary<string, string>()
         {
             {"exit", "exit"},
